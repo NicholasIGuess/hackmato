1144ca243a36b30861338110657062d370e1cc54
NicholasIGuess <114117177+NicholasIGuess@users.noreply.github.com>
2024-11-02 15:47:24 -0400

    did some stuff

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ var timer = pomodoro;
 var currentPhase = 0;
 var numBreaks = 0;
 var breakAmount = 3;
+var timerInterval;
+
+var startBtn = document.getElementById("start");
+var pauseBtn = document.getElementById("pause");
+var resetBtn = document.getElementById("reset");
 
 function secondsToTime(seconds) {
     var m = Math.floor(seconds / 60);
@@ -16,10 +21,14 @@ function secondsToTime(seconds) {
 }
 
 function start() {
+    if (timerInterval) {
+        clearInterval(timerInterval);
+    }
     timer = pomodoro;
     currentPhase = 0;
     document.getElementById("count").innerHTML = secondsToTime(timer);
     document.getElementById("phase").innerHTML = "get to work!";
+    document.getElementsByClassName("timer")[0].style.backgroundColor = "#ff4747";
     timerInterval = setInterval(() => {
         timer--;
         document.getElementById("count").innerHTML = secondsToTime(timer);
@@ -43,6 +52,12 @@ function start() {
             }
 
             document.getElementById("phase").innerHTML = currentPhase == 0 ? "get to work!" : currentPhase == 1 ? "take a break!" : "nice, a long break!";
+            var color = currentPhase == 0 ? "#ff4747" : currentPhase == 1 ? "#88e788" : "#47a9ff";
+            document.getElementsByClassName("timer")[0].style.backgroundColor = currentPhase == 0 ? "#ff4747" : currentPhase == 1 ? "#88ff88" : "#47a9ff";
+            for (let i = 0; i < 3; i++) {
+                document.getElementsByClassName("playback")[i].style.backgroundColor = currentPhase == 0 ? "#ff4747" : currentPhase == 1 ? "#88e788" : "#90d5ff";
+            }
+            startBtn.removeEventListener("mouseenter", start);
             document.getElementById("count").innerHTML = secondsToTime(timer);
         }
     }, 1000);
