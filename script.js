9aa8b484982cf4f26ae268079fa74bdb4313bca3
NicholasIGuess <114117177+NicholasIGuess@users.noreply.github.com>
2024-11-02 22:05:37 -0400

    settings

--- script.js
+++ script.js
@@ -27,11 +27,13 @@ function start() {
     timer = pomodoro;
     currentPhase = 0;
     document.getElementById("count").innerHTML = secondsToTime(timer);
-    document.getElementById("phase").innerHTML = "get to work!";
+    document.getElementById("phase").innerHTML = "started!";
     document.getElementsByClassName("timer")[0].style.backgroundColor = "#ff4747";
     timerInterval = setInterval(() => {
         timer--;
         document.getElementById("count").innerHTML = secondsToTime(timer);
+        document.getElementById("phase").innerHTML = currentPhase == 0 ? "get to work!" : currentPhase == 1 ? "take a break!" : "nice, a long break!";
+        document.title = secondsToTime(timer) + " - hackmato";
         if (timer == 0) {
             if (currentPhase == 0) {
                 if (numBreaks == breakAmount) {
@@ -52,13 +54,82 @@ function start() {
             }
 
             document.getElementById("phase").innerHTML = currentPhase == 0 ? "get to work!" : currentPhase == 1 ? "take a break!" : "nice, a long break!";
-            var color = currentPhase == 0 ? "#ff4747" : currentPhase == 1 ? "#88e788" : "#47a9ff";
             document.getElementsByClassName("timer")[0].style.backgroundColor = currentPhase == 0 ? "#ff4747" : currentPhase == 1 ? "#88ff88" : "#47a9ff";
-            for (let i = 0; i < 3; i++) {
-                document.getElementsByClassName("playback")[i].style.backgroundColor = currentPhase == 0 ? "#ff4747" : currentPhase == 1 ? "#88e788" : "#90d5ff";
-            }
-            startBtn.removeEventListener("mouseenter", start);
             document.getElementById("count").innerHTML = secondsToTime(timer);
         }
     }, 1000);
+}
+
+function pause() {
+    if (timerInterval != null) {
+        clearInterval(timerInterval);
+        timerInterval = null;
+        document.getElementById("phase").innerHTML = "paused :(";
+        document.title = "paused - hackmato";
+        document.getElementById("pause").innerHTML = "resume";
+    } else {
+        document.getElementById("phase").innerHTML = "resumed!";
+        document.getElementById("pause").innerHTML = "pause";
+        timerInterval = setInterval(() => {
+            timer--;
+            document.getElementById("phase").innerHTML = currentPhase == 0 ? "get to work!" : currentPhase == 1 ? "take a break!" : "nice, a long break!";
+            document.getElementById("count").innerHTML = secondsToTime(timer);
+            document.title = secondsToTime(timer) + " - hackmato";
+            if (timer == 0) {
+                if (currentPhase == 0) {
+                    if (numBreaks == breakAmount) {
+                        currentPhase = 2;
+                        timer = longBreak;
+                        numBreaks = 0;
+                    } else {
+                        currentPhase = 1;
+                        timer = shortBreak;
+                        numBreaks++;
+                    }
+                } else if (currentPhase == 1) {
+                    currentPhase = 0;
+                    timer = pomodoro;
+                } else if (currentPhase == 2) {
+                    currentPhase = 0;
+                    timer = pomodoro;
+                }
+    
+                document.getElementById("phase").innerHTML = currentPhase == 0 ? "get to work!" : currentPhase == 1 ? "take a break!" : "nice, a long break!";
+                document.getElementsByClassName("timer")[0].style.backgroundColor = currentPhase == 0 ? "#ff4747" : currentPhase == 1 ? "#88ff88" : "#47a9ff";
+                document.getElementById("count").innerHTML = secondsToTime(timer);
+            }
+        }, 1000);
+    }
+}
+
+function reset() {
+    clearInterval(timerInterval);
+    timer = pomodoro;
+    currentPhase = 0;
+    document.getElementById("count").innerHTML = secondsToTime(timer);
+    document.getElementById("phase").innerHTML = "press start!";
+    document.title = "hackmato";
+    document.getElementsByClassName("timer")[0].style.backgroundColor = "#ff4747";
+    timerInterval = null;
+    numBreaks = 0;
+}
+
+function openSettings() {
+    if (document.getElementsByClassName("invisible")[0].style.display == "block") {
+        for (var i = 0; i < document.getElementsByClassName("invisible").length; i++) {
+            document.getElementsByClassName("invisible")[i].style.display = "none";
+        }
+    } else {
+        for (var i = 0; i < document.getElementsByClassName("invisible").length; i++) {
+            document.getElementsByClassName("invisible")[i].style.display = "block";
+        }
+    }
+}
+
+function update() {
+    pomodoro = parseInt(document.getElementById("work").value) * 60;
+    shortBreak = parseInt(document.getElementById("break").value) * 60;
+    longBreak = parseInt(document.getElementById("rest").value) * 60;
+    breakAmount = parseInt(document.getElementById("breaks").value);
+    reset();
 }
